2cf596630ac20adfad75e698c2f8cb913ae4de08
viktor <radchenkovictor@yahoo.com>
2021-01-12 20:58:35 +0400

    updated user model

--- app/models/user.py
+++ app/models/user.py
@@ -11,11 +11,16 @@ class User(db.Model, UserMixin, ModelMixin):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.Text, unique=True)
+    username = db.Column(db.Text)  # TODO: set up JWT auth using email
     email = db.Column(db.Text, unique=True)
     password = db.Column(db.String(256))
-    roles = db.Column(db.Text)
-    is_active = db.Column(db.Boolean, default=True, server_default='true')
+    phone = db.Column(db.String(32), unique=True)
+    address = db.Column(db.Text)
+    city = db.Column(db.String(64))
+    country = db.Column(db.String(64))
+    zip_code = db.Column(db.String(16))
+    roles = db.Column(db.Text, default='user')
+    activated = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime, default=datetime.now)
 
     @property
@@ -38,7 +43,7 @@ class User(db.Model, UserMixin, ModelMixin):
         return self.id
 
     def is_valid(self):
-        return self.is_active
+        return self.activated
 
     def __str__(self):
         return '<User: %s>' % self.username
