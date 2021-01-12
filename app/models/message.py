26eb94a92971d7dd436dd34e9575ee91b0a0eb97
viktor <radchenkovictor@yahoo.com>
2021-01-12 21:43:54 +0400

    message model

---
+++ app/models/message.py
@@ -0,0 +1,20 @@
+from app import db
+from app.models.utils import ModelMixin
+
+
+class Message(db.Model, ModelMixin):
+
+    __tablename__ = 'messages'
+
+    message_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128), nullable=False)
+    phone_number = db.Column(db.String(32), nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    lat = db.Column(db.String(32))
+    lon = db.Column(db.String(32))
+    ip_address = db.Column(db.String(16))
+    zip_code = db.Column(db.String(16))
+    tag_id = db.Column(db.Integer, db.ForeignKey("tags.tag_id"))
+
+    def __str__(self):
+        return '<Message: %d>' % self.message_id
